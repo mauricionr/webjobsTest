4522b2154f1e7599a1323a8977cd9d40348dc3b2
markusahlstrand <markus@ahlstrand.de>
2014-09-03 14:50:40 +0200

    Added a console app

---
+++ App_Data/Jobs/Continuous/testConsole/testConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            { 
+                Console.WriteLine("I'm c#..");
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
