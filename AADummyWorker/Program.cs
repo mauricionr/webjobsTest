bbf73f553a70a105107a582ff88244704cd6d755
markusahlstrand <markus@ahlstrand.de>
2014-09-04 19:28:39 +0200

    Added a new dummy webjob with references to the other webjobs

---
+++ AADummyWorker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AADummyWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Dummy worker");
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
